e675e4a4dcb218d7891e5510495fa86fae9fa9a3
Mario Osvaldo Nuñez <marionumza@gmail.com>
2021-11-19 09:42:13 -0300

    Update __manifest__.py

--- cocacola_default/__manifest__.py
+++ cocacola_default/__manifest__.py
@@ -1,94 +1,91 @@
+##############################################################################
+#
+#    Copyright (C) 2021  Quilsoft  (http://www.quilsoft.com)
+#    All Rights Reserved.
+#
+##############################################################################
+
 {
     'name': 'cocacola',
     'version': '14.0.1.0.0',
     'category': 'Tools',
-    'summary': 'Proyecto Cocacola',
-    'author': 'Quilsoft',
+    'summary': "Proyect module for Coca Cola",
+    'author': "Quilsoft",
+    'website': 'quilsoft.com',
+    'license': 'AGPL-3',
     'depends': [
-    ],
-    'data': [
-    ],
-    'test': [
-    ],
+            # Modulos odoo
+            'crm',
+            'project',
+            'stock',
+            'mrp',
+            'sale_management',
+            'account_accountant',
+            'purchase',
+            'hr_attendance',
+            'contacts',
+#            'calendar',
+#            'account',
+            # Modulos quilsoft
+
+
+        ],
     'installable': True,
-    'application': False,
+
     'env-ver': '2',
-    'odoo-license': 'CE',
-    'port': '8069',
+    'odoo-license': 'EE',
     'config': [
-        'max_cron_threads = 1',
+
+        # 'addons_path' is always computed looking for the repositories in sources
+        # 'data_dir' is a fixed location inside docker odoo image
+
+        # You should use 2 worker threads + 1 cron thread per available CPU,
+        # and 1 CPU per 10 concurent users.
+        # if ommited oe will calculate workers and cron´s based on # of cpu
+        #        'workers = 0',
+                'max_cron_threads = 1',
+
+        # Number of requests a worker will process before being recycled and
+        # restarted. Defaults to 8192 if ommited
+                'limit_request = 8192',
+
+        # Maximum allowed virtual memory per worker. If the limit is exceeded,
+        # the worker is killed and recycled at the end of the current request.
+        # Defaults to 640MB
+                'limit_memory_soft = 2147483648',
+
+        # Hard limit on virtual memory, any worker exceeding the limit will be
+        # immediately killed without waiting for the end of the current request
+        # processing. Defaults to 768MB.
+                'limit_memory_hard = 2684354560',
     ],
+
+    'port': '8069',
+
     'git-repos': [
-        'https://github.com/jobiols/cl-cocacola.git',
-        # todos juntos
-        # ==========================================================================================
-        # 'https://github.com/OCA/account-closing oca-account-closing',
-        'https://github.com/OCA/account-financial-reporting oca-account-financial-reporting',  # noqa
-        'https://github.com/OCA/account-financial-tools oca-account-financial-tools',
-        'https://github.com/OCA/account-payment oca-account-payment',
-        # 'https://github.com/OCA/apps-store oca-apps-store',
-        # 'https://github.com/OCA/bank-payment oca-bank-payment',
-        # 'https://github.com/OCA/business-requirement oca-business-requirement',
-        # 'https://github.com/OCA/commission oca-commission',
-        # 'https://github.com/OCA/contract oca-contract',
-        # 'https://github.com/OCA/credit-control oca-credit-control',
-        # 'https://github.com/OCA/crm oca-crm',
-        # 'https://github.com/OCA/currency oca-currency',
-        # 'https://github.com/OCA/ddmrp oca-ddmrp',
-        # 'https://github.com/OCA/delivery-carrier oca-delivery-carrier',
-        # 'https://github.com/OCA/e-commerce oca-e-commerce',
-        # 'https://github.com/OCA/event oca-event',
-        # 'https://github.com/OCA/field-service oca-field-service',
-        # 'https://github.com/OCA/geospatial oca-geospatial',
-        # 'https://github.com/OCA/hr oca-hr',
-        # 'https://github.com/OCA/hr-timesheet oca-hr-timesheet',
-        # 'https://github.com/OCA/knowledge oca-knowledge',
-        # 'https://github.com/OCA/management-system oca-management-system',
-        # 'https://github.com/OCA/manufacture oca-manufacture',
-        # 'https://github.com/OCA/manufacture-reporting oca-manufacture-reporting',
-        # 'https://github.com/OCA/margin-analysis oca-margin-analysis',
-        # 'https://github.com/OCA/multi-company oca-multi-company',
-        # 'https://github.com/OCA/oca-custom oca-oca-custom',
-        # 'https://github.com/OCA/operating-unit oca-operating-unit',
-        # 'https://github.com/OCA/partner-contact oca-partner-contact',
-        # 'https://github.com/OCA/pos oca-pos',
-        # 'https://github.com/OCA/product-attribute oca-product-attribute',
-        # 'https://github.com/OCA/product-pack oca-product-pack',
-        # 'https://github.com/OCA/project oca-project',
-        # 'https://github.com/OCA/project-reporting oca-project-reporting',
-        # 'https://github.com/OCA/purchase-workflow oca-purchase-workflow',
-        # 'https://github.com/OCA/queue oca-queue',
-        # 'https://github.com/OCA/report-print-send oca-report-print-send',
-        'https://github.com/OCA/reporting-engine oca-reporting-engine',
-        # 'https://github.com/OCA/sale-reporting oca-sale-reporting',
-        'https://github.com/OCA/sale-workflow oca-sale-workflow',
-        # 'https://github.com/OCA/server-auth oca-server-auth',
-        # 'https://github.com/OCA/server-backend oca-server-backend',
-        'https://github.com/OCA/server-tools oca-server-tools',
-        'https://github.com/OCA/server-ux oca-server-ux',
-        # 'https://github.com/OCA/social oca-social',
-        # 'https://github.com/OCA/stock-logistics-barcode oca-stock-logistics-barcode',
-        # 'https://github.com/OCA/stock-logistics-reporting oca-stock-logistics-reporting', # noqa
-        # 'https://github.com/OCA/stock-logistics-transport oca-stock-logistics-transport', # noqa
-        # 'https://github.com/OCA/stock-logistics-warehouse oca-stock-logistics-warehouse', # noqa
-        # 'https://github.com/OCA/stock-logistics-workflow oca-stock-logistics-workflow',
-        # 'https://github.com/OCA/timesheet oca-timesheet',
-        # 'https://github.com/OCA/vertical-association oca-vertical-association',
-        'https://github.com/OCA/web oca-web',
-        # 'https://github.com/OCA/website oca-website',
-        # 'https://github.com/OCA/bank-payment oca-bank-payment',
-        # 'https://github.com/OCA/account-analytic',
-        # 'https://github.com/ingadhoc/account-analytic ingadhoc-account-analytic',
-        # 'https://github.com/ingadhoc/hr ingadhoc-hr',
-        # 'https://github.com/ingadhoc/multi-company ingadhoc-multi-company',
-        # 'https://github.com/ingadhoc/multi-store ingadhoc-multi-store',
+        'git@github.com:jobiols/cl-cocacola.git',
 
-        # Fix porque falla la instalacion de l10n_ar_ux
+        # Quilsoft
+        #'git@github.com:quilsoft-org/cocacola.git -b main',
 
-        # 'https://github.com/jobiols/odoo-argentina jobiols-odoo-argentina',
+        # OCA
+        # 'https://github.com/OCA/brand oca-brand',
+        # 'https://github.com/OCA/web oca-web',
+        # 'https://github.com/OCA/server-ux.git oca-server-ux',
+
+        # Quilsoft-Org
+        # 'https://github.com/ingadhoc/account-financial-tools adhoc-account-financial-tools',
+        # 'https://github.com/ingadhoc/account-payment adhoc-account-payment',
+        # 'https://github.com/ingadhoc/aeroo_reports.git',
+        # 'https://github.com/ingadhoc/argentina-sale adhoc-argentina-sale',
+        # 'https://github.com/ingadhoc/odoo-argentina adhoc-odoo-argentina',
+        # 'https://github.com/ingadhoc/odoo-argentina-ee adhoc-odoo-argentina-ee',
+        # 'https://github.com/ingadhoc/stock adhoc-stock',
     ],
+
+    # list of images to use in the form 'name image-url'
     'docker-images': [
-        'odoo jobiols/odoo-jeo:14.0',
+        'odoo jobiols/odoo-ent:14.0e',
         'postgres postgres:10.1-alpine',
     ]
 }
